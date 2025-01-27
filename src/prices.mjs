4fe829e427932b46b8cce0b54e717913d3a714f2
anastat <anastasiia.tikhonova@mail.com>
2025-01-27 21:30:54 +0200

    Removed Date and parseDate function

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,17 +19,10 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
     const plainDate = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, plainDate, baseCost);
     res.json({ cost });
   });
-
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
-  }
 
   function parsePlainDate(dateString) {
     if (dateString) {
